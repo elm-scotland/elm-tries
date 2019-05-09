c63c85b4565417e3acafa2b6bdc6796a2a9a67c5
Rupert Smith <rupert@thesett.com>
2019-05-09 16:49:47 +0100

    Added test case for a list of keys.

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,7 +1,8 @@
-module Example exposing (emptyContainsNoVal, singletonContainsVal)
+module Example exposing (emptyContainsNoVal, listOfValsContainsAllVals, singletonContainsVal, singletonEmptyStringContainsVal)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Set
 import Test exposing (..)
 import Trie
 
@@ -9,7 +10,7 @@ import Trie
 emptyContainsNoVal : Test
 emptyContainsNoVal =
     describe "emptyContainsNoVal"
-        [ fuzz string "creates singleton tries" <|
+        [ fuzz string "Creates singleton tries." <|
             \val ->
                 Trie.empty |> Trie.get val |> Expect.equal Nothing
         ]
@@ -18,7 +19,31 @@ emptyContainsNoVal =
 singletonContainsVal : Test
 singletonContainsVal =
     describe "singletonContainsVal"
-        [ fuzz string "creates singleton tries" <|
+        [ fuzz string "Creates singleton tries." <|
             \val ->
                 Trie.singleton val val |> Trie.get val |> Expect.equal (Just val)
+        ]
+
+
+singletonEmptyStringContainsVal : Test
+singletonEmptyStringContainsVal =
+    describe "singletonEmptyStringContainsVal"
+        [ fuzz string "Creates singleton tries with the empty string as key." <|
+            \val ->
+                Trie.singleton "" val |> Trie.get "" |> Expect.equal (Just val)
+        ]
+
+
+listOfValsContainsAllVals : Test
+listOfValsContainsAllVals =
+    describe "listOfValsContainsAllVals"
+        [ fuzz (list string) "Creates a trie with a list of vals and ensures it contains all of them." <|
+            \possiblyEmptyVals ->
+                case possiblyEmptyVals of
+                    [] ->
+                        Expect.equal [] []
+
+                    vals ->
+                        List.foldl (\val trie -> trie) Trie.empty vals
+                            |> Expect.all (List.map (\val trie -> Trie.get val trie |> Expect.equal (Just val)) vals)
         ]
